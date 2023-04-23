65d8cae0a0bdee8b63e97bb0d2aafc5151ab4bbd
EseniyaKodenko <130905665+EseniyaKodenko@users.noreply.github.com>
2023-04-23 19:09:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_1samp
+from statsmodels.stats.weightstats import ztest
 
 
 chat_id = 1019285902 # Ваш chat ID, не меняйте название переменной
 
 def solution(dt) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    pvalue = ttest_1samp(dt, popmean = 300)[1]
+    pvalue = ztest(dt, value = 300, alternative="smaller")[1]
     ans = True if pvalue < 0.1 else False
     return ans # Ваш ответ, True или False
+
