41ec86831cdd908def796cdf8e66b4e1ba2b25d6
EseniyaKodenko <130905665+EseniyaKodenko@users.noreply.github.com>
2023-04-23 18:40:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1019285902 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(dt) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    pvalue = ttest_1samp(dt, popmean = 300)[1]
+    ans = True if pvalue < 0.1 else False
+    return ans # Ваш ответ, True или False
